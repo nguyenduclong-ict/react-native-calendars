2bdaea0f77848a0020052cacd3ed80a4a8c66d58
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2021-05-15 16:49:03 +0700

    update

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -53,11 +53,8 @@ class CalendarHeader extends Component {
     webAriaLevel: 1
   };
 
-  headerComponent;
-
   constructor(props) {
     super(props);
-    this.headerComponent = props.headerComponent;
 
     this.style = styleConstructor(props.theme);
   }
@@ -229,7 +226,7 @@ class CalendarHeader extends Component {
         <View style={this.style.header}>
           {this.renderArrow('left')}
           <View style={this.style.headerContainer}>
-            {this.props.headerComponent ? <this.props.headerComponent month={this.props.month} /> : this.renderHeader()}
+            {this.renderHeader()}
             {this.renderIndicator()}
           </View>
           {this.renderArrow('right')}
